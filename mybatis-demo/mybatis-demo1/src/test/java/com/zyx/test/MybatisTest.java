d0e85ae8edce4ab896153c4a62e30a4be4620e99
zyx-pt <zyx_pt@163.com>
2020-05-09 11:30:40 +0800

    test 添加注释

--- mybatis-demo/mybatis-demo1/src/test/java/com/zyx/test/MybatisTest.java
+++ mybatis-demo/mybatis-demo1/src/test/java/com/zyx/test/MybatisTest.java
@@ -18,20 +18,27 @@ import java.util.List;
  */
 public class MybatisTest {
     public static void main(String[] args) throws Exception{
+        //1.读取配置文件
         InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
 
+        //2.创建 SqlSessionFactory 的构建者对象
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
 
+        //3.使用构建者创建工厂对象 SqlSessionFactory
         SqlSessionFactory factory = builder.build(in);
 
+        //4.使用 SqlSessionFactory 生产 SqlSession 对象
         SqlSession session = factory.openSession();
 
-        UserMapper userDao = session.getMapper(UserMapper.class);
+        //5.使用 SqlSession 创建 mapper 接口的代理对象
+        UserMapper userMapper = session.getMapper(UserMapper.class);
 
-        List<User> users = userDao.findAll();
+        //6.使用代理对象执行查询所有方法
+        List<User> users = userMapper.findAll();
         for (User user : users) {
             System.out.println(user);
         }
+        //7.释放资源
         session.close();
         in.close();
     }
