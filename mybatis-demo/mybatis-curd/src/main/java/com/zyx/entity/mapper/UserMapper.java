1641b5dd9e389af165e9854fc3548cc5090d8e78
zyx-pt <zyx_pt@163.com>
2020-05-19 17:46:04 +0800

    添加CURD-demo

---
+++ mybatis-demo/mybatis-curd/src/main/java/com/zyx/entity/mapper/UserMapper.java
@@ -0,0 +1,66 @@
+package com.zyx.entity.mapper;
+
+import com.zyx.entity.model.QueryVo;
+import com.zyx.entity.model.User;
+
+import java.util.List;
+
+/**
+ * 用户持久层接口
+ * @author zhengyongxian
+ * @date 2020/4/1 22:18
+ */
+public interface UserMapper {
+    /**
+     * 查询所有用户
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据id删除用户
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据名称模糊查询用户信息
+     * @param username
+     * @return
+     */
+    List<User> findByName(String username);
+
+    /**
+     * 查询总用户数
+     * @return
+     */
+    int findTotal();
+
+    /**
+     * 根据queryVo中的条件查询用户
+     * @param vo
+     * @return
+     */
+    List<User> findUserByVo(QueryVo vo);
+
+
+}
---
+++ mybatis-demo/mybatis-curd/src/main/java/com/zyx/entity/model/QueryVo.java
@@ -0,0 +1,18 @@
+package com.zyx.entity.model;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ */
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
